57e3b7c80999d1eca41c6e1358154740dad7b056
Austin Ziegler <austin@zieglers.ca>
2014-12-21 23:40:43 -0500

    Improve documentation.

--- lib/archive/tar/minitar.rb
+++ lib/archive/tar/minitar.rb
@@ -2,10 +2,10 @@
 
 require 'fileutils'
 
-module Archive
-  module Tar
+module Archive #:nodoc:
+  module Tar #:nodoc:
     class << self
-      def const_missing(const)
+      def const_missing(const) #:nodoc:
         case const
         when :PosixHeader
           warn "Archive::Tar::PosixHeader has been renamed to Archive::Tar::Minitar::PosixHeader"
@@ -29,8 +29,46 @@ module Archive
       end
     end
 
+    # == Synopsis
+    #
+    # Using minitar is easy. The simplest case is:
+    #
+    #     require 'zlib'
+    #     require 'minitar'
+    #
+    #     # Packs everything that matches Find.find('tests').
+    #     # test.tar will automatically be closed by Minitar.pack.
+    #     Minitar.pack('tests', File.open('test.tar', 'wb'))
+    #
+    #     # Unpacks 'test.tar' to 'x', creating 'x' if necessary.
+    #     Minitar.unpack('test.tar', 'x')
+    #
+    # A gzipped tar can be written with:
+    #
+    #     # test.tgz will be closed automatically.
+    #     Minitar.pack('tests', Zlib::GzipWriter.new(File.open('test.tgz', 'wb'))
+    #
+    #     # test.tgz will be closed automatically.
+    #     Minitar.unpack(Zlib::GzipReader.new(File.open('test.tgz', 'rb')), 'x')
+    #
+    # As the case above shows, one need not write to a file. However, it will
+    # sometimes require that one dive a little deeper into the API, as in the
+    # case of StringIO objects. Note that I'm not providing a block with
+    # Minitar::Output, as Minitar::Output#close automatically closes both the
+    # Output object and the wrapped data stream object.
+    #
+    #     begin
+    #       sgz = Zlib::GzipWriter.new(StringIO.new(""))
+    #       tar = Output.new(sgz)
+    #       Find.find('tests') do |entry|
+    #         Minitar.pack_file(entry, tar)
+    #       end
+    #     ensure
+    #         # Closes both tar and sgz.
+    #       tar.close
+    #     end
     module Minitar
-      VERSION = '0.6'
+      VERSION = '0.6' # :nodoc:
 
       # Raised when a wrapped data stream class is not seekable.
       NonSeekableStream = Class.new(StandardError)
