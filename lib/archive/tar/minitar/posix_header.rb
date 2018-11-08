9a99f66a6edd9e95b25ff0d6c6d5381916c7e01b
Savater Sebastien <savater.sebastien@gmail.com>
2018-11-08 11:29:53 +0100

    Refactor strict octal handling to allow spaces
    -   This fixes the problem of
    
        ```
        > Minitar.unpack(io, path)
        ArgumentError: " 1213" is not a valid octal string
        ```
    
        These files are provided by the french administration and can be extracted
        by various softwares.
        Example:
          https://cadastre.data.gouv.fr/data/dgfip-pci-vecteur/latest/edigeo/feuilles/52/52053/edigeo-520530000A04.tar.bz2
    
    -   Add specs on Minitar.unpack about non strict octal
    
    Created-by: inkstak
    Resolves #35

--- lib/archive/tar/minitar/posix_header.rb
+++ lib/archive/tar/minitar/posix_header.rb
@@ -123,7 +123,7 @@ class Archive::Tar::Minitar::PosixHeader
     private
 
     def strict_oct(string)
-      return string.oct if string =~ /\A[0-7]*\z/
+      return string.oct if string =~ /\A[0-7 ]*\z/
       raise ArgumentError, "#{string.inspect} is not a valid octal string"
     end
   end
