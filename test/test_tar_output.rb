90829cf6dec576a0295f5ce2bf6e7bd563cd753e
Austin Ziegler <austin@zieglers.ca>
2024-11-12 12:44:29 -0500

    fix: Broken tests

--- test/test_tar_output.rb
+++ test/test_tar_output.rb
@@ -32,11 +32,11 @@ class TestTarOutput < Minitest::Test
   def test_file_looks_good
     Minitar::Output.open(@tarfile) do |os|
       NAMES.each do |name|
-        name = File.join("data__", name)
-        stat = File.stat(name)
+        filename = File.join("data__", name)
+        stat = File.stat(filename)
         opts = {size: stat.size, mode: 0o644}
         os.tar.add_file_simple(name, opts) do |ss|
-          File.open(name, "rb") { |ff| ss.write(ff.read(4096)) until ff.eof? }
+          File.open(filename, "rb") { |ff| ss.write(ff.read(4096)) until ff.eof? }
         end
       end
     end
