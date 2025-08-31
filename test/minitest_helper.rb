cac3ab06c6b3bfa6889e22c5a128fcd544ccc6e0
Austin Ziegler <austin@zieglers.ca>
2025-08-31 19:47:53 -0400

    chore: Make minitest_helper more comprehensive
    Improve strict mode handling and support file loading.
    
    Signed-off-by: Austin Ziegler <austin@zieglers.ca>

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,10 +1,22 @@
+# frozen_string_literal: true
+
+require "base64"
 require "fileutils"
 require "minitar"
+require "pathname"
+require "stringio"
+require "zlib"
 
 gem "minitest"
 require "minitest/autorun"
 require "minitest/focus"
 
-Dir.glob(File.join(File.dirname(__FILE__), "support/*.rb")).sort.each do |support|
+if ENV["STRICT"] != "false"
+  $VERBOSE = true
+  Warning[:deprecated] = true
+  require "minitest/error_on_warning"
+end
+
+Dir.glob(File.join(__dir__, "support/**/*.rb")).sort.each do |support|
   require support
 end
